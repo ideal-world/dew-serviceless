4217f89436d5017e0d24af2a58c286c080424baa
gudaoxuri <i@sunisle.org>
2020-12-01 22:55:06 +0800

    Start Integration Testing.

---
+++ sources/services/gateway/src/test/java/idealworld/dew/serviceless/gateway/test/TestGatewayApplication.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020. gudaoxuri
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package idealworld.dew.serviceless.gateway.test;
+
+import idealworld.dew.serviceless.gateway.GatewayApplication;
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxTestContext;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TestGatewayApplication extends BasicTest {
+
+    @SneakyThrows
+    @Test
+    public void start(Vertx vertx, VertxTestContext testContext) {
+        System.getProperties().put("dew.profile", "test");
+        vertx.deployVerticle(new GatewayApplication(), testContext.succeedingThenComplete());
+        new CountDownLatch(1).await();
+    }
+
+}
